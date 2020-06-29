49685972b10dc53dbe7d8714a459a27e599da90b
User <vartan.andreevich@yandex.ru>
2020-06-29 21:49:03 +0300

    6.0.2. Заполнение массива. [#311689]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Andre Tsarukyan";
+        names[2] = "Korobeinikov Stas";
+        names[3] = "Sergey Mayer";
+
+        System.out.println("Petr Arsentev");
+        System.out.println("Andre Tsarukyan");
+        System.out.println("Korobeinikov Stas");
+        System.out.println("Sergey Mayer");
     }
 }
