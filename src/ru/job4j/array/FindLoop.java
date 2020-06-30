41d89539fef28a603d02ff50a31f1811e8a08231
User <vartan.andreevich@yandex.ru>
2020-06-30 21:32:41 +0300

    6.4.2. Слово заканчивается на ... [#311681]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,6 +1,17 @@
 package ru.job4j.array;
 
 public class FindLoop {
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (; start <= finish; start++) {
+            if (data[start] == el) {
+                rst = start;
+                break;
+            }
+        }
+        return rst;
+    }
+
     public static int indexOf(int[] data, int el) {
         int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
         for (int index = 0; index < data.length; index++) {
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -23,4 +23,37 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind2() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 10;
+        int start = 0;
+        int finish = 2;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNoFind6() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 6;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
